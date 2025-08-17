ef20b549792cdab082c63e3c245d3170d6c2cdd9
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2025-08-17 20:54:15 +0800

    feat: Remove serviceWorker

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,6 @@ import './index.css';
 import './assets/clear.css';
 import './assets/font.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 // Simple approach: Just make sure the DOM is ready before rendering
 // This is the standard way React handles this in Create React App
@@ -14,19 +13,4 @@ if (rootElement) {
   ReactDOM.render(<App />, rootElement);
 } else {
   console.error('Root element not found. Make sure there is a div with id="root" in your HTML.');
-}
-
-serviceWorker.unregister();
-
-if (module.hot && !window.frameElement) {
-  console.log('HMR enabled');
-  module.hot.accept('./App', () => {
-    const NextApp = require('./App').default;
-    const rootElement = document.getElementById('root');
-    if (rootElement) {
-      ReactDOM.render(<NextApp />, rootElement);
-    } else {
-      console.error('Root element not found during hot reload.');
-    }
-  });
 }
