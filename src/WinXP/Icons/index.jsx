84444c67b720c44bbe9d3ab6795997d209c90c57
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2025-08-24 12:38:00 +0800

    refactor: Refactoring styled component into module CSS

--- src/WinXP/Icons/index.jsx
+++ src/WinXP/Icons/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
-import styled from 'styled-components';
+import clsx from 'clsx';
+
+import styles from './styles.module.scss'
 
 function Icons({
   icons,
@@ -30,18 +32,21 @@ function Icons({
     setSelectedIcons(selectedIds);
   }, [iconsRect, setSelectedIcons, selecting, mouse.docX, mouse.docY]);
   return (
-    <IconsContainer>
+    <div className={styles.root}>
       {icons.map(icon => (
-        <StyledIcon
+        <Icon
           key={icon.id}
           {...icon}
-          displayFocus={displayFocus}
+          className={clsx(
+            styles.icon,
+            icon.isFocus &&displayFocus && styles['displayFocus'],
+          )}
           onMouseDown={onMouseDown}
           onDoubleClick={onDoubleClick}
           measure={measure}
         />
       ))}
-    </IconsContainer>
+    </div>
   );
 }
 
@@ -72,72 +77,21 @@ function Icon({
   }, [id, measure]);
   return (
     <div
-      className={className}
+      className={clsx(
+        className,
+      )}
       onMouseDown={_onMouseDown}
       onDoubleClick={_onDoubleClick}
       ref={ref}
     >
-      <div className={`${className}__img__container`}>
-        <img src={icon} alt={title} className={`${className}__img`} />
+      <div className={styles[`icon__img__container`]}>
+        <img src={icon} alt={title} className={styles[`icon__img`]} />
       </div>
-      <div className={`${className}__text__container`}>
-        <div className={`${className}__text`}>{title}</div>
+      <div className={styles[`icon__text__container`]}>
+        <div className={styles[`icon__text`]}>{title}</div>
       </div>
     </div>
   );
 }
-
-const IconsContainer = styled.div`
-  position: absolute;
-  margin-top: 40px;
-  margin-left: 40px;
-`;
-
-const StyledIcon = styled(Icon)`
-  width: 70px;
-  margin-bottom: 30px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  &__text__container {
-    width: 100%;
-    font-size: 10px;
-    color: white;
-    text-shadow: 0 1px 1px black;
-    margin-top: 5px;
-    display: flex;
-    justify-content: center;
-
-    &:before {
-      content: '';
-      display: block;
-      flex-grow: 1;
-    }
-    &:after {
-      content: '';
-      display: block;
-      flex-grow: 1;
-    }
-  }
-  &__text {
-    padding: 0 3px 2px;
-    background-color: ${({ isFocus, displayFocus }) =>
-      isFocus && displayFocus ? '#0b61ff' : 'transparent'};
-    text-align: center;
-    flex-shrink: 1;
-  }
-  &__img__container {
-    width: 30px;
-    height: 30px;
-    filter: ${({ isFocus, displayFocus }) =>
-      isFocus && displayFocus ? 'drop-shadow(0 0 blue)' : ''};
-  }
-  &__img {
-    width: 30px;
-    height: 30px;
-    opacity: ${({ isFocus, displayFocus }) =>
-      isFocus && displayFocus ? 0.5 : 1};
-  }
-`;
 
 export default Icons;
